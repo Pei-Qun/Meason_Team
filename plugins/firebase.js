ee412852adbc876afabd7d990c751dbb650356c2
曾霈宭 <nancy@gmail.com>
2020-06-26 22:22:30 +0800

    firebase fix

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -4,13 +4,13 @@ import * as firebase from 'firebase/app'
 // import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDgJMzNmBVVBoIdbmaUe1ijBAMBmgRl1ew:',
-  authDomain: 'meason-music.firebaseapp.com:',
-  databaseURL: 'https://meason-music.firebaseio.com:',
-  projectId: 'meason-music:',
-  storageBucket: 'meason-music.appspot.com:',
-  messagingSenderId: '433097675711:',
-  appId: '1:433097675711:web:07d45c297e318a7ec1070a:'
+  apiKey: 'AIzaSyDgJMzNmBVVBoIdbmaUe1ijBAMBmgRl1ew',
+  authDomain: 'meason-music.firebaseapp.com',
+  databaseURL: 'https://meason-music.firebaseio.com',
+  projectId: 'meason-music',
+  storageBucket: 'meason-music.appspot.com',
+  messagingSenderId: '433097675711',
+  appId: '1:433097675711:web:07d45c297e318a7ec1070a'
 }
 
 if (!firebase.apps.length) {
