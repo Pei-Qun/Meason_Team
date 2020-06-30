60c986776a48dd8f4cee3f0da1619bca3ebd5542
曾霈宭 <nancy@gmail.com>
2020-06-30 09:40:48 +0800

    id update

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -24,8 +24,22 @@
             <h4>匿名</h4>
           </article>
           <article>
+            <p>投稿類型</p>
+            <h4>{{ data.classify }}</h4>
+          </article>
+          <article>
+            <p>發布日期</p>
+            <h4>{{ time }}</h4>
+          </article>
+          <article>
+            <p>投稿狀態</p>
+            <h4 v-if="data.status === 'unpublish'" class="badge badge-info">未引用</h4>
+            <h4 v-if="data.status === 'publish'" class="badge badge-success">已引用</h4>
+            <h4 v-if="data.status === 'reject'" class="badge badge-danger">已違規</h4>
+          </article>
+          <article>
             <p>貼文內容</p>
-            <h4>{{ data.content }}</h4>
+            <pre>{{ data.content }}</pre>
           </article>
         </div>
       </section>
@@ -44,6 +58,36 @@ export default {
   computed: {
     pyAPI () {
       return this.$store.state.pyAPI
+    },
+    time () {
+      const dateTimeStamp = this.data.timestamp
+      const minute = 1000 * 60
+      const hour = minute * 60
+      const day = hour * 24
+      const month = day * 30
+      const now = new Date().getTime()
+      const diffValue = now - dateTimeStamp
+      if (diffValue < 0) { return }
+      const monthC = diffValue / month
+      const weekC = diffValue / (7 * day)
+      const dayC = diffValue / day
+      const hourC = diffValue / hour
+      const minC = diffValue / minute
+      let result = ''
+      if (monthC >= 1) {
+        result = '' + parseInt(monthC) + '月前'
+      } else if (weekC >= 1) {
+        result = '' + parseInt(weekC) + '周前'
+      } else if (dayC >= 1) {
+        result = '' + parseInt(dayC) + '天前'
+      } else if (hourC >= 1) {
+        result = '' + parseInt(hourC) + '小時前'
+      } else if (minC >= 1) {
+        result = '' + parseInt(minC) + '分鐘前'
+      } else {
+        result = '剛剛'
+      }
+      return result
     }
   },
   methods: {
